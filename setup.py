9c4fa9e8dacc9ba7f9f3d06009862ce11e1a40d8
Ashwin Das <ashwindasr@users.noreply.github.com>
2021-09-02 12:45:05 -0400

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hw2b',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     license='none',
     description='Building a CI pipeline for hw 2b',
